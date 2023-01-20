790c711d77e801594942f0b4705a4f65b029f4e8
Oskar Kaptacz <oskarkaptacz@gmail.com>
2023-01-21 00:32:10 +0100

    low stock fix

--- src/lib/util/lowStock.ts
+++ src/lib/util/lowStock.ts
@@ -1,5 +1,5 @@
 import { ProductVariant } from "@medusajs/medusa"
 
 export const lowStock = (variant: Omit<ProductVariant, "beforeInsert">) => {
-  return variant.inventory_quantity > 6 && variant.inventory_quantity != null 
+  return variant.inventory_quantity > 6 && variant.inventory_quantity != null && variant.inventory_quantity < 0
 }
